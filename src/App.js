0ccc986721128c08fbb3d226aea89463be3719ac
jangidjay <jangid321jay@gmail.com>
2022-05-21 10:14:36 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,87 +1,106 @@
-import { useState, useEffect } from "react";
 import "./App.css";
-import { db } from "./firebase-config";
-import {
-  collection,
-  getDocs,
-  addDoc,
-  deleteDoc,
-  doc,
-} from "firebase/firestore";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
 
 function App() {
-  const [newEmail, setNewEmail] = useState("");
-  const [newName, setNewName] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-
-  const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users");
-
-  const createUser = async () => {
-    await addDoc(usersCollectionRef, {
-      email: newEmail,
-      name: newName,
-      password: newPassword,
-    });
-  };
-
-  const deleteUser = async (id) => {
-    const userDoc = doc(db, "users", id);
-    await deleteDoc(userDoc);
-  };
-
-  useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-
-    getUsers();
-  }, []);
-
   return (
-    <div className="App">
-      <input
-        type="email"
-        placeholder="Email..."
-        onChange={(event) => {
-          setNewEmail(event.target.value);
-        }}
-      />
-      <input
-        placeholder="Name..."
-        onChange={(event) => {
-          setNewName(event.target.value);
-        }}
-      />
-      <input
-        type="password"
-        placeholder="Password..."
-        onChange={(event) => {
-          setNewPassword(event.target.value);
-        }}
-      />
-      <button onClick={createUser}>SignUP</button>
-
-      {users.map((user) => {
-        return (
-          <div>
-            {" "}
-            <h1>Email: {user.email}</h1>
-            <h1>Name: {user.name}</h1>
-            <h1>Password: {user.password}</h1>
-            <button
-              onClick={() => {
-                deleteUser(user.id);
-              }}
-            >
-              Delete User
-            </button>
-          </div>
-        );
-      })}
+    <div className="app">
+      <Router>
+        <Routes>
+          {/* <Route exact path="/" component={Login} /> */}
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+// import { useState, useEffect } from "react";
+// import "./App.css";
+// import { db } from "./firebase-config";
+// import {
+//   collection,
+//   getDocs,
+//   addDoc,
+//   deleteDoc,
+//   doc,
+// } from "firebase/firestore";
+
+// function App() {
+//   const [newEmail, setNewEmail] = useState("");
+//   const [newName, setNewName] = useState("");
+//   const [newPassword, setNewPassword] = useState("");
+
+//   const [users, setUsers] = useState([]);
+//   const usersCollectionRef = collection(db, "users");
+
+//   const createUser = async () => {
+//     await addDoc(usersCollectionRef, {
+//       email: newEmail,
+//       name: newName,
+//       password: newPassword,
+//     });
+//   };
+
+//   const deleteUser = async (id) => {
+//     const userDoc = doc(db, "users", id);
+//     await deleteDoc(userDoc);
+//   };
+
+//   useEffect(() => {
+//     const getUsers = async () => {
+//       const data = await getDocs(usersCollectionRef);
+//       setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+//     };
+
+//     getUsers();
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <input
+//         type="email"
+//         placeholder="Email..."
+//         onChange={(event) => {
+//           setNewEmail(event.target.value);
+//         }}
+//       />
+//       <input
+//         placeholder="Name..."
+//         onChange={(event) => {
+//           setNewName(event.target.value);
+//         }}
+//       />
+//       <input
+//         type="password"
+//         placeholder="Password..."
+//         onChange={(event) => {
+//           setNewPassword(event.target.value);
+//         }}
+//       />
+//       <button onClick={createUser}>SignUP</button>
+
+//       {users.map((user) => {
+//         return (
+//           <div>
+//             {" "}
+//             <h1>Email: {user.email}</h1>
+//             <h1>Name: {user.name}</h1>
+//             <h1>Password: {user.password}</h1>
+//             <button
+//               onClick={() => {
+//                 deleteUser(user.id);
+//               }}
+//             >
+//               Delete User
+//             </button>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,22 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyClT9H6iSQ98oHYVll9VbyqRLEYnRNAAvA",
@@ -12,5 +29,76 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
 
-export const db = getFirestore();
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(name, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password Reser Link Sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  logInWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordResetEmail,
+  logout,
+};
